f4fe0ad51bc83712c341e47da9a04604885b5ff0
Snehanjali Kalamkar <ksnehanjali@gmail.com>
2020-03-15 23:10:54 +0530

    Added login, logout functionality - SessionsController

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum:15 }
+  validates :username, presence: true, uniqueness: true, case_sensitive: false, length: { minimum: 3, maximum:15 }
   has_secure_password
   has_many :messages
 end
