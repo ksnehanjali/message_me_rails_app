5d5a6aa8b06f5eda1486e250a362114bb98e8433
Snehanjali Kalamkar <ksnehanjali@gmail.com>
2020-03-15 19:33:59 +0530

    Added Messages model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: 'Sneha', password: 'sneha123')
+User.create(username: 'Neha', password: 'neha123')
+User.create(username: 'Ankit', password: 'ankit123')
